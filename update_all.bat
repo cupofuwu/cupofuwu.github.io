03e6cf87c561a6b8bc92cb93a5afb036913116e3
cupofuwu <uwu@uwu.com>
2025-05-10 18:54:25 +0300

    Auto-update all

---
+++ update_all.bat
@@ -0,0 +1,19 @@
+@echo off
+echo Updating all to GitHub...
+
+:: Добавление файла index.html
+git add :/
+
+git add -u
+
+git checkout HEAD -- update_all.bat
+git checkout HEAD -- update_index.bat
+
+:: Создание коммита с автоматическим сообщением
+git commit -m "Auto-update all" || echo No changes to commit
+
+:: Отправка изменений в ветку main
+git push origin main
+
+:: Пауза для просмотра результата (опционально)
+pause
